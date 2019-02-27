cd93bf77e6695b434c17b212c1c8f9f287176573
Daniel Hovorka <dhovorka10@gmail.com>
2019-02-26 19:18:44 -0600

    added seed for model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# item = ModelName.new({attribute_1: “some value”, attribute_2: “some value”})
+#item.save
+
+item = Product.new({name: "Tennis Balls", price: 3, image_url: "https://en.wikipedia.org/wiki/File:Roland_Garros_2012_Ballkid.jpg", description: "These are great, but only if you like tennis. If you don't they're sort of boring"})
+item.save
