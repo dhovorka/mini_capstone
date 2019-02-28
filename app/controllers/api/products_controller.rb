f3ceacb4b28cc38c23242933956841945f4c29c4
Daniel Hovorka <dhovorka10@gmail.com>
2019-02-27 21:14:14 -0600

    added params and removed extra jbuilder files

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,17 +5,49 @@ class Api::ProductsController < ApplicationController
   end
 
   def product_one
-    @one = Product.first
-    render "product_one.json.jbuilder"
+    @stuff = Product.first
+    render "product_list.json.jbuilder"
   end
 
   def product_three
-    @three = Product.third
-    render "product_three.json.jbuilder"
+    @stuff = Product.third
+    render "product_list.json.jbuilder"
   end
-end
 #   def view_contact
 #     @person = Contact.all
 #     render "view_contact.json.jbuilder"
 #   end
 # end
+
+# @stuff = Product.find_by(id: product_id)
+# This is preffered for larger numbers of product, this current answwer only works with 
+# low overall product numbers 
+
+def input_id
+  id = params["added_id"].to_i
+  if id == 1
+    @stuff = Product.first
+    render "product_list.json.jbuilder"
+  elsif id == 2
+    @stuff = Product.second
+    render "product_list.json.jbuilder"
+  elsif id == 3
+    @stuff = Product.third
+    render "product_list.json.jbuilder"
+  end
+end
+
+def input_id_segment
+  id = params["my_id"].to_i
+  if id == 1
+    @stuff = Product.first
+    render "product_list.json.jbuilder"
+  elsif id == 2
+    @stuff = Product.second
+    render "product_list.json.jbuilder"
+  elsif id == 3
+    @stuff = Product.third
+    render "product_list.json.jbuilder"
+  end
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
     get "/view_product" => "products#product_list"
     get "/product_one" => "products#product_one"
     get "/product_three" => "products#product_three"
+
+    get "/product" => "products#input_id"
+    get "/product/:my_id" => 'products#input_id_segment'
   end
   end
