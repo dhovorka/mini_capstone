8dc409cfa6f725b33da78e95b942dff168fe9b8f
Daniel Hovorka <dhovorka10@gmail.com>
2019-03-20 19:14:53 -0500

    added edit & update routes/methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,4 +24,22 @@ class ProductsController < ApplicationController
     #render "show.html.erb"
     redirect_to "/products/#{@product.id}"
   end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    if @product.save
+      render "show.html.erb"
+    else
+    #render json: {message: "not saved, what did you do wrong?"}#redirect_to "/products"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get "/products/new" => "products#new"
   post "/products" => "products#create" 
   get "/products/:id" => "products#show"
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
 
   namespace :api do
     post "/carted_products" => "carted_products#create"
